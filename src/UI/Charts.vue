767496c67188210f92a818dd053e356ddf12b2b1
akas salvar <asalvaruulu@gmail.com>
2023-02-22 13:54:53 +0600

    feat: added header

---
+++ src/UI/Charts.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full h-fit bg-white rounded-[10px] md:w-[calc(50%-5px)]">
+    <div
+      class="flex rounded-t-[10px] justify-evenly gap-x-[10px] w-full bg-green-100 py-[22px] border-b-2 border-border"
+    >
+      <p class="text-xs text-secondary">Last Month</p>
+      <p class="text-xs text-secondary">3M</p>
+      <p class="text-xs text-secondary">6M</p>
+      <p class="text-xs text-secondary">Year</p>
+      <p class="text-xs text-secondary">All</p>
+    </div>
+    <div class="w-full bg-pink-400 h-[285px] rounded-b-[10px]"></div>
+  </div>
+</template>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="dark container text-primary">
+  <section class="dark container text-primary">
     <ul class="font-bold">
       <li class="text-primary bg-primary-light p-4">primary</li>
       <li class="text-success bg-success-light p-4">success</li>
@@ -14,10 +14,70 @@
         >Переключить тему</span
       >
     </button>
+  </section>
+  <header id="header" class="h-[100px] w-[100%] border-y-2 border-border">
+    <div class="container h-full">
+      <div class="flex flex-wrap items-center h-full justify-between">
+        <div
+          class="flex-col flex justify-center items-center rounded-[50%] w-[50px] h-[50px] bg-primary gap-y-[3px]"
+        >
+          <div class="h-[2px] w-[18px] bg-side rounded-[1px]"></div>
+          <div class="h-[2px] w-[18px] bg-side rounded-[1px]"></div>
+          <div class="h-[2px] w-[18px] bg-side rounded-[1px]"></div>
+        </div>
+        <div class="flex items-center gap-x-[20px]">
+          <h2
+            class="mr-[80px] text-secondary text-[18px] xl:mx-[40px] 2xl:ml-[0px] 2xl:mr-[80px]"
+          >
+            Constructor
+          </h2>
+          <nav class="hidden xl:flex">
+            <ul class="flex gap-x-[20px] mr-[20px]">
+              <NavItem />
+            </ul>
+          </nav>
+        </div>
+        <Input />
+        <div class="flex items-center">
+          <div
+            class="rounded-[50%] w-[50px] h-[50px] bg-primary flex items-center justify-center md:bg-transparent"
+          >
+            <img src="../assets/images/avatar.svg" alt="" />
+          </div>
+          <p
+            class="hidden text-[13px] ml-[10px] text-secondary mr-[30px] md:flex"
+          >
+            Clayton Santos
+          </p>
+          <div
+            class="rounded-[50%] w-[50px] h-[50px] bg-primary hidden items-center justify-center md:flex"
+          >
+            <img src="../assets/images/loop.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
   </header>
+  <section class="w-[100%] h-[calc(100vh-100px)]" id="panel">
+    <div class="container h-full w-full flex">
+      <div class="hidden flex-col gap-y-[20px] pr-[23px] pt-[30px] xl:flex">
+        <NavIcon />
+      </div>
+      <div class="flex py-[30px] w-full gap-x-[10px] gap-y-[20px] flex-wrap">
+        <Charts />
+        <Charts />
+        <Charts />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
+import NavItem from "../components/NavItem.vue";
+import NavIcon from "../components/NavIcon.vue";
+import Input from "../UI/Input.vue";
+import Charts from "../UI/Charts.vue";
+
 function toggleTheme() {
   if (document.documentElement.getAttribute("data-mode") === "dark") {
     document.documentElement.setAttribute("data-mode", null);
