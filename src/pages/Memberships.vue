0e2b949e324516d0be030f7b9dde1ddda37f2d35
Estebes <estebesm95@gmail.com>
2023-02-28 09:51:25 +0600

    feat: added tel input

--- src/pages/Memberships.vue
+++ src/pages/Memberships.vue
@@ -9,36 +9,13 @@
         <div
           class="bg-side py-5 px-4 md:px-8 rounded-lg w-[calc(100vw-32px)] max-w-[400px]"
         >
-          <form class="grid grid-cols-1 gap-y-3">
-            <h3 class="text-xl mb-4">Добавить абонемент</h3>
-            <Textfield
-              label="Номер абонемента"
-              type="number"
-              placeholder="12345"
-              class="w-full"
-            />
-            <Textfield label="Имя" class="block w-full" placeholder="Иван" />
-            <Textfield
-              label="Фамилия"
-              class="block w-full"
-              placeholder="Иванов"
-            />
-            <div class="flex justify-end gap-x-4 mt-2">
-              <button type="button">Отмена</button>
-              <Button
-                type="button"
-                class="bg-primary rounded py-2 px-4 text-btn"
-              >
-                Добавить
-              </Button>
-            </div>
-          </form>
+          <MembershipForm />
         </div>
       </Modal>
     </div>
     <MembershipsTable />
     <div class="flex justify-end">
-      <button class="hover:text-primary transition font-[400]">
+      <button class="mt-2 hover:text-primary transition font-[400]">
         Скачать excel
       </button>
     </div>
@@ -51,8 +28,8 @@ import { useHead } from "@vueuse/head";
 import DatePicker from "@/components/DatePicker.vue";
 import MembershipsTable from "@/components/tables/MembershipsTable.vue";
 import Button from "@/ui/Button.vue";
-import Textfield from "@/ui/Textfield.vue";
 import Modal from "@/components/Modal.vue";
+import MembershipForm from "@/components/forms/MembershipForm.vue";
 useHead({
   title: "Абонементы",
 });
--- src/ui/Textfield.vue
+++ src/ui/Textfield.vue
@@ -2,7 +2,7 @@
   <div>
     <label v-if="label" class="block text-sm mb-2">{{ label }}</label>
     <input
-      class="py-2 px-2 outline-offset-0 rounded bg-main outline-none focus:outline-primary placeholder:font-[400]"
+      class="py-2 px-2 outline-offset-0 rounded bg-main outline-none border border-secondary dark:border-main focus:outline-primary placeholder:font-[400]"
       v-bind="$attrs"
     />
   </div>
