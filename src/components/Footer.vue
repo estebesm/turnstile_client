a13e34316c305a9f9134b26163ac035d1c9d2e1a
Estebes <estebesm95@gmail.com>
2023-03-01 20:34:28 +0600

    fix: changed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,103 +1,98 @@
 <template>
-  <footer class="hidden lg:block">
-    <div class="w-full h-[40px] bg-side border-b-2 border-icon"></div>
+  <footer
+    class="hidden mt-10 bg-gray-700 dark:bg-zinc-900 text-gray-300 lg:block pt-20 shadow-[0px_-2px_4px_rgba(0,0,0,0.2)] dark:shadow-[0px_-2px_4px_rgba(0,0,0,0.1)]"
+  >
     <div
-      class="flex-col bg-main shadow-md px-[60px] bottom-0 left-0 right-0 hidden justify-between items-center lg:flex"
+      class="grid grid-cols-[3fr_3fr_3fr] xl:grid-cols-[3fr_2fr_2fr_2fr] gap-x-10 mx-auto container"
     >
-      <div
-        class="flex mt-[-20px] rounded-b-lg items-center bg-side justify-center pb-[20px] w-full gap-x-10 border-b-2 border-x-2 border-icon"
-      >
-        <nav>
-          <ul class="hidden lg:flex gap-x-8">
-            <li>
-              <button
-                class="transition rounded-sm px-2"
-                :class="{ 'text-primary': route.name === 'dashboard' }"
-                @click="() => router.push('/dashboard')"
-              >
-                <span>Журнал</span>
-              </button>
-            </li>
-            <li>
-              <button
-                class="transition rounded-sm px-2"
-                :class="{ 'text-primary': route.name === 'memberships' }"
-                @click="() => router.push('/memberships')"
-              >
-                <span>Абонементы</span>
-              </button>
-            </li>
-            <li>
-              <button
-                class="transition rounded-sm px-2"
-                :class="{ 'text-primary': route.name === 'info' }"
-              >
-                <span>Информация</span>
-              </button>
-            </li>
-            <li>
-              <button
-                class="transition rounded-sm px-2"
-                :class="{ 'text-primary': route.name === 'settings' }"
-              >
-                <span>Настройки</span>
-              </button>
-            </li>
-          </ul>
-        </nav>
+      <div class="h-full flex flex-col justify-center mr-10">
+        <h1 class="text-[30px] whitespace-nowrap">Turnstile Api</h1>
       </div>
-      <div class="flex w-full justify-evenly py-[20px]">
-        <div class="flex flex-col justify-center w-[25%] xl:w-[20%]">
-          <Logo />
-          <p class="text-center mt-[10px]">Пользовательское соглашение</p>
+      <div class="hidden xl:flex flex-col gap-y-2">
+        <h4 class="text-lg mb-4">Продукт</h4>
+        <div>
+          <a
+            href="https://google.com"
+            target="_blank"
+            class="text-gray-300 font-[400]"
+            >Журнал</a
+          >
         </div>
-        <div class="flex flex-col justify-center w-[25%] xl:w-[20%]">
-          <h3 class="text-center">О нас</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Sit,aperiam.
-          </p>
+        <div>
+          <a
+            href="https://google.com"
+            target="_blank"
+            class="text-gray-300 font-[400]"
+            >Абонементы</a
+          >
+        </div>
+        <div>
+          <a
+            href="https://google.com"
+            target="_blank"
+            class="text-gray-300 font-[400]"
+            >Информация</a
+          >
+        </div>
+        <div>
+          <a
+            href="https://google.com"
+            target="_blank"
+            class="text-gray-300 font-[400]"
+            >Настройки</a
+          >
         </div>
       </div>
-      <div
-        class="flex rounded-t-lg bg-side justify-center pt-[10px] w-full border-x-2 border-t-2 border-icon"
-      >
-        <h3>Контакты:</h3>
+      <div class="flex flex-col gap-y-2">
+        <h4 class="text-lg mb-4">Компания</h4>
+        <div>
+          <a
+            href="https://google.com"
+            target="_blank"
+            class="text-gray-300 font-[400]"
+            >Novasoft</a
+          >
+        </div>
+        <div>
+          <a
+            href="https://google.com"
+            target="_blank"
+            class="text-gray-300 font-[400] whitespace-nowrap"
+            >Пользовательское соглашение</a
+          >
+        </div>
+        <div>
+          <a
+            href="https://google.com"
+            target="_blank"
+            class="text-gray-300 font-[400]"
+            >О продукте</a
+          >
+        </div>
       </div>
-      <div
-        class="flex mb-[-20px] w-full bg-side justify-center gap-x-6 border-x-2 border-icon"
-      >
-        <div class="flex justify-center items-center">
-          <img
-            class="w-[30px] h-[30px] mr-[5px]"
-            src="https://cdn2.iconfinder.com/data/icons/social-media-2420/512/Telegram-512.png"
-            alt=""
-          />
-          <p>telegram</p>
-        </div>
-        <div class="flex justify-center items-center">
-          <img
-            class="w-[30px] h-[30px] mr-[5px]"
-            src="https://cdn2.iconfinder.com/data/icons/social-media-2420/512/WhatsApp-512.png"
-            alt=""
-          />
-          <p>whatsapp</p>
-        </div>
-        <div class="flex flex-col justify-center">
-          <p>0555888999</p>
-        </div>
-        <div class="flex flex-col justify-center">
-          <p>0500222111</p>
+      <div class="flex flex-col gap-y-2">
+        <h4 class="text-lg mb-4">Контакты</h4>
+        <TelNumber number="+996 995 123 130" />
+        <TelNumber number="+996 995 123 130" />
+        <EmailAdress email="estebesm95@gmail.com" />
+        <div class="flex items-center gap-x-2">
+          <a href="https://t.me/estebesm">
+            <img src="@/assets/icons/telegram.svg" alt="telegram" />
+          </a>
+          <a href="https://wa.me/+996995123130">
+            <img src="@/assets/icons/whatsapp.svg" alt="telegram" />
+          </a>
         </div>
       </div>
     </div>
-    <div class="w-full h-[40px] bg-side border-t-2 border-icon"></div>
+    <p class="text-center text-sm font-[400] mt-20 pb-4">Бишкек 2023</p>
   </footer>
 </template>
 
 <script setup>
 import { useRoute, useRouter } from "vue-router";
-import Logo from "@/ui/Logo.vue";
+import TelNumber from "@/ui/TelNumber.vue";
+import EmailAdress from "@/ui/EmailAdress.vue";
 const route = useRoute();
 const router = useRouter();
 </script>
