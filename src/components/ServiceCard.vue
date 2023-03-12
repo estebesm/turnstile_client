d5443e232be91252e4a025218b95665bd25a86e7
Estebes Mukambetov <estebesm95@gmail.com>
2023-03-12 14:52:31 +0600

    feat: addeforms for trainers and sevice cards

---
+++ src/components/ServiceCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="py-4 px-4 md:py-4 md:px-8 rounded bg-gray-200 dark:bg-side">
+    <div class="flex justify-between items-start gap-x-4">
+      <h3 class="font-[600] text-lg">Эстебес</h3>
+      <div class="flex gap-x-1">
+        <EditButton />
+        <DeleteButton />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import EditButton from "@/ui/EditButton.vue";
+import DeleteButton from "@/ui/DeleteButton.vue";
+</script>
---
+++ src/components/forms/trainer_forms/CreateTrainerForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <form
+    class="grid grid-cols-1 gap-y-3 bg-side py-5 px-4 md:px-8 rounded-lg w-[calc(100vw-32px)] max-w-[400px]"
+  >
+    <h3 class="text-xl mb-4">Добавить тренера</h3>
+    <Textfield
+      v-model:value="state.first_name"
+      title="Имя"
+      placeholder="Иван"
+      class="w-full"
+    />
+    <Textfield
+      v-model:value="state.last_name"
+      title="Фамилия"
+      placeholder="Иванов"
+      class="w-full"
+    />
+    <Textfield
+      v-model:value="state.patronymic"
+      title="Отчество"
+      placeholder="Иванович"
+      class="w-full"
+    />
+    <div class="flex justify-end gap-x-4 mt-2">
+      <Button type="button" @click="$emit('close-modal')">Отмена</Button>
+      <Button
+        type="button"
+        class="bg-primary rounded py-2 px-4 text-btn"
+        :class="{ 'bg-primary-light': state.loading }"
+        :disabled="state.loading"
+        @click="submit"
+      >
+        Создать
+      </Button>
+    </div>
+  </form>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import Button from "@/ui/Button.vue";
+import Textfield from "@/ui/Textfield.vue";
+
+const emit = defineEmits(["close-modal"]);
+
+const state = reactive({
+  first_name: "",
+  last_name: "",
+  patronymic: "",
+});
+
+function submit() {
+  emit("close-modal");
+}
+</script>
